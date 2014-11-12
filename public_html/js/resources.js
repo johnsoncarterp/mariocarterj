541d64f617a64e7bd3eccc2b0b70b5ac96412a9c
carter johnson <carter32lax@yahoo.com>
2014-11-12 13:56:45 -0800

    finished video 6 and have mario

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -4,8 +4,8 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-
 {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+{name: "mario", type:"image", src: "data/img/player1.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -16,7 +16,6 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-
     {name: "carterlevel01", type: "tmx", src: "data/map/carterlevel01.tmx"}
 	/* Background music. 
 	 * @example
