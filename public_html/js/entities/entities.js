388058738e8eebffe80e28d1cce7612b4027336b
carter johnson <carter32lax@yahoo.com>
2014-11-14 13:59:06 -0800

    on minuit 10 on video 9

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -11,18 +11,29 @@ game.PlayerEntity = me.Entity.extend({
                   return (new me.Rect(0, 0, 128, 128)).toPolygon();  
                 }
         }]);
+    
+    this.renderable.addAnimation("idle", [3]);
+    this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80); 
+    
+    this.renderable.setCurrentAnimation("idle");
        
-       this.body.setVelocity(5, 0);
+       this.body.setVelocity(5, 20);
     },
     
     update: function(delta){
         if(me.input.isKeyPressed("right")) {
-           this.body.vel.x += this.body.accel.x * me.timer.tick; 
+           this.body.vel.x += this.body.accel.x * me.timer.tick;
+           this.renderable.setCurrentAnimation("smallWalk");
         }else{
             this.body.vel.x = 0;
         }
         
+        if(this.body.vel.x !== 0){
+            
+        }
+        
         this.body.update(delta);
+        this._super(me.Entity, "update", [delta]);
         return true;
     }
     
