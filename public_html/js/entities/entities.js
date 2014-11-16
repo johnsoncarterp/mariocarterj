ee44d14e9b1e4bc576052fa8c65296ceec00995b
carter johnson <carter32lax@yahoo.com>
2014-11-16 13:17:15 -0800

    finished video 9

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -23,13 +23,18 @@ game.PlayerEntity = me.Entity.extend({
     update: function(delta){
         if(me.input.isKeyPressed("right")) {
            this.body.vel.x += this.body.accel.x * me.timer.tick;
-           this.renderable.setCurrentAnimation("smallWalk");
+           
         }else{
             this.body.vel.x = 0;
         }
         
         if(this.body.vel.x !== 0){
-            
+            if(!this.renderable.isCurrentAnimation("smallwalk")) {
+                this.renderable.setCurrentAnimation("smallWalk");
+                this.renderable.setAnimationFrame();
+            }
+        }else{
+            this.renderable.setCurrentAnimation("idle");
         }
         
         this.body.update(delta);
