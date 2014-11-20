28a469c60272716ce7c8455a98c3488221e1923d
carter johnson <carter32lax@yahoo.com>
2014-11-20 13:56:06 -0800

    done with video 11

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -18,6 +18,7 @@ game.PlayerEntity = me.Entity.extend({
     this.renderable.setCurrentAnimation("idle");
        
        this.body.setVelocity(5, 20);
+       me.viewPort.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
     
     update: function(delta){
@@ -48,6 +49,8 @@ game.PlayerEntity = me.Entity.extend({
         }
         
         
+        
+        
         this._super(me.Entity, "update", [delta]);
         return true;
     },
@@ -71,6 +74,7 @@ game.LevelTrigger = me.Entity.extend({
     onCollision: function(){ 
        this.body.setCollisionMask(me.collision.types.NO_OBJECT);
        me.levelDirector.loadLevel(this.level);
+       me.state.current().resetPlayer();
     }
     
 });
